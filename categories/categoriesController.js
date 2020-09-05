e5692bb00fd5088bd1b57ddbd905cc5219fb3de9
Jeferson Ryan <50426159+JefersonRyan@users.noreply.github.com>
2020-09-05 15:19:35 -0300

    Conclusão das categorias

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -2,7 +2,8 @@ const express = require('express');
 const slugify = require('slugify');
 const  Router = express.Router(); 
 const ModelCategory =  require('./ModelCategory');
-const category = require('./ModelCategory');
+
+
 
 
 Router.get('/admim/categories/new',(req,res)=>{
@@ -34,6 +35,65 @@ Router.get('/admim/categories',(req,res)=>{
     })
 
 });
+
+Router.post('/categories/delete',(req,res)=>{
+    const id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            ModelCategory.destroy({
+                where: {
+                    id:id
+                }
+            }).then(()=>{
+                return res.redirect("/admim/categories");
+            });
+
+        }else{
+            return res.redirect("/admim/categories");
+        }
+    }else{
+        return res.redirect("/admim/categories");
+    }
+});
+
+
+Router.get('/admim/categories/edit/:id',(req,res)=>{
+    let id = req.params.id;
+    
+    if(isNaN(id)){
+        return res.redirect('/admim/categories')
+    }
+    ModelCategory.findByPk(id).then((category)=>{
+        if(category != undefined){
+            return res.render('admim/categories/edit',{category: category});
+        }else{
+            return res.redirect('/admim/categories');
+        }
+    }).catch((err)=>{
+        return res.redirect('/admim/categories');
+    });
+});
+
+
+
+Router.post('/admim/categories/update',(req,res)=>{
+        const id = req.body.id;
+        const title = req.body.title;
+
+        ModelCategory.update({title:title,
+                              slug: slugify(title)},{  
+             where: {
+                id:id
+            }
+    }).then(()=>{
+            return res.redirect('/admim/categories');
+        }).catch((err)=>{
+
+        });
+
+});
+
+
 
 module.exports = Router;
 
