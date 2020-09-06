8564c2a0e55de040f594e46d2a18a98942123791
Jeferson Ryan <50426159+JefersonRyan@users.noreply.github.com>
2020-09-06 19:43:25 -0300

    Inicio dos artigos

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const Router =  express.Router();
+const modelCategory = require('../categories/ModelCategory');
 
 
 Router.get(('/articles'),(req,res)=>{
     return res.send('Articles');
+})
+
+Router.get(('/admim/articles/new'),(req,res)=>{
+    
+    modelCategory.findAll().then(categories =>{
+
+        return res.render('admim/articles/new',{categories:categories});
+    }).catch((err)=>{
+        return res.redirect('admim/articles');
+    });
+
 })
 
 
--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const connection = require('./database/connection');
 const categoriesRouter =  require('./categories/categoriesController');
 const articlesRouter = require('./articles/articlesController');
-const ModelArticle =  require('./articles/ModelArticle');
-const MoldelCaregory = require('./categories/ModelCategory');
+
 
 const app = express();
 
@@ -29,8 +28,6 @@ connection
         }).catch((error)=>{
             console.log(error);
         });
-
-
 
 
 // Routes
